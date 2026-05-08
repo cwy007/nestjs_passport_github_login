703aeaf6370fa6305f2f27c0d48dffd42e9aab90
chanweiyan <chanweiyan007@gmail.com>
2026-05-08 14:10:01 +0800

    fix: http://localhost:3000/callback\?code\=e16d72e87de71b732ce5 这里的 code 有什么用

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,7 +22,7 @@ export class AppController {
   @UseGuards(AuthGuard('github'))
   getCallback(@Query('code') code: string, @Req() req: Request) {
     console.log('User profile:', req.user);
-    console.log('Authorization code:', code);
+    console.log('Authorization code:', code); // 在 passport-github2 中，授权码通常不会直接传递到回调函数中，而是由库内部处理并交换为访问令牌。
     return this.appService.findUserByGithubId(req.user!.id);
   }
 }
